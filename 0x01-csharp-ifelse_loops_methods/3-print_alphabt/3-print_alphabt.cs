fbd9890aaefc67b92a38e4190996f993b2d37748
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 03:25:27 -0600

    task 3

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 97; i < 123; i++)
+            {
+                if ((i != 101) && (i != 113))
+                {
+                    Console.Write("{0}", (char)i);
+                }
+            }
+        }
+    }
+}
