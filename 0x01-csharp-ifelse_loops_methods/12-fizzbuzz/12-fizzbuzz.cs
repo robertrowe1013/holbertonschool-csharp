ccb4f43099f9243370c5d33a8b508acdb237fa2f
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 17:24:11 -0600

    fizzbuzz

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 100; i++)
+            {
+                if (((i % 3) == 0) && ((i % 5) == 0))
+                {
+                    Console.Write("FizzBuzz");
+                }
+                else if ((i % 3) == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                else
+                {
+                    Console.Write("{0}", i);
+                }
+                if (i < 100)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
