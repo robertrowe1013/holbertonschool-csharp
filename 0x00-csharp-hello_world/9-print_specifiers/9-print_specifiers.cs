52fb9c104f553b401a8628b58b8f267b2f6f9005
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-11-30 20:25:20 -0600

    changed percentage

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,6 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}%\nCurrency: {1}", (percent * 100).ToString("F"), currency.ToString("C2"));
+		Console.WriteLine("Percent: {0}\nCurrency: {1}", (percent).ToString("P"), currency.ToString("C2"));
 	}
 }
