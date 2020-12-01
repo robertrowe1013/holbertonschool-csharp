6091a3419db4c43f5aa9d239865477e3cea2a671
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 16:04:36 -0600

    task 7

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (((char)c > 96) && ((char)c < 123))
+        {
+            return true;
+        }
+        return false;
+    }
+}
