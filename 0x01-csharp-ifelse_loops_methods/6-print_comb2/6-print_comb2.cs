5b088d717f1ef2bcfdf54f2bd711d11e9a8884be
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 12:36:12 -0600

    task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tens = 0;
+            int ones = 1;
+            while (tens < 9)
+            {
+                while (ones < 10)
+                {
+                    Console.Write("{0}{1}", tens, ones);
+                    if ((tens == 8) && (ones == 9))
+                    {
+                        Console.Write("\n");
+                    }
+                    else
+                    {
+                        Console.Write(", ");
+                    }
+                    ones++;
+                }
+                tens++;
+                ones = tens + 1;
+            }
+        }
+    }
+}
