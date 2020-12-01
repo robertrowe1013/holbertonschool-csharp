3610e6d63b1c10ddb6fac5e2f417efa57aaafb2c
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 17:14:27 -0600

    task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length > 0)
+        {
+            int i = 0;
+            int spaces;
+            while (i < length)
+            {
+                for (spaces = 0; spaces < i; spaces++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\\n");
+                i++;
+            }
+            Console.WriteLine("\n");
+        }
+        Console.Write("\n");
+    }
+}
