fdc61742329bd225f66facc08ca46b39c6526974
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 03:18:23 -0600

    task 2

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 97; i < 123; i++)
+            {
+                Console.Write("{0}", (char)i);
+            }
+        }
+    }
+}
