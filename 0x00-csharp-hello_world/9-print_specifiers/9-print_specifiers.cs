1ab3881c37c3426be8c2ac9e8e7fbf01a1b32af7
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-11-30 20:18:11 -0600

    task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		Console.WriteLine("Percent: {0}\nCurrency: {1}", (percent * 100).ToString("F"), currency.ToString("C2"));
+	}
+}
