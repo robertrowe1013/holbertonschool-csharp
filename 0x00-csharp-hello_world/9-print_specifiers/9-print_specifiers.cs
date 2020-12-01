9153234d3dd0a2294d38035a3a8436da981cf514
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-11-30 20:31:13 -0600

    task 9 again

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,6 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0}\nCurrency: {1}", (percent).ToString("P"), currency.ToString("C2"));
+		Console.WriteLine("Percent: {0}", percent.ToString("P2"));
+        Console.WriteLine("Currency: {0}", currency.ToString("C2"));
 	}
 }
