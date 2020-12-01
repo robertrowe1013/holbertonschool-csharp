f8639088da499cb8a26895511740d4ee1a676289
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-01 16:59:43 -0600

    task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+        for (i = 0; i < length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.Write("\n");
+    }
+}
