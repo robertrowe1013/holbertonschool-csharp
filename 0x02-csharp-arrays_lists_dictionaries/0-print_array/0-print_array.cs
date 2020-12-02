3b5203c2d8fabfd552a96d5617f10df3c911a49a
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 04:14:36 -0600

    task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if (size == 0)
+        {
+            int[] newArray = new int[0];
+            Console.Write("\n");
+            return newArray;
+        }
+        else
+        {
+            int[] newArray = new int[size];
+            int i;
+            for (i = 0; i < size; i++)
+            {
+                newArray[i] = i;
+            }
+            return newArray;
+        }
+    }
+}
