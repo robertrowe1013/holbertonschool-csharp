741e8a02df3d6bc4b2f421edcc75257d26842c51
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 13:14:38 -0600

    renamed max var

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if ((myList == null) || (myList.Count == 0))
+        {
+            Console.WriteLine("List is empty");
+            return -1;
+        }
+        int i;
+        int maxInt;
+        
+        maxInt = myList[0];
+        for (i = 0; i < (myList.Count - 1); i++)
+        {
+            if (myList[i] > maxInt)
+            {
+                maxInt = myList[i];
+            }
+        }
+
+        return maxInt;
+    }
+}
