4259c2ee8307d15ecf679c748a52a322cd753768
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 13:01:13 -0600

    corrected array range check

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -4,7 +4,7 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if ((index > array.Length) || (index < 0))
+        if ((index > array.Length - 1) || (index < 0))
         {
             Console.WriteLine("Index out of range");
             return array;
