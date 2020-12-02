037b86a6955e7a8eb4a6d5427af2eceb40b0f91c
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 12:28:06 -0600

    task 4

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        List<int> printList = new List<int>();
+        int i;
+        if (size == 0)
+        {
+            Console.Write("\n");
+        }
+        for (i = 0; i < size; i++)
+        {
+            printList.Add(i);
+            Console.Write(i);
+            if (i == (size - 1))
+            {
+                Console.Write("\n");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+        }
+
+        return printList;
+    }
+}
