eecacd3dfe9e7e954530a47051630f9c7e7d456d
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 05:14:24 -0600

    task 3

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if ((array == null) || (array.Length == 0))
+        {
+            Console.Write("\n");
+        }
+        else
+        {
+            int i = (array.Length - 1);
+            while (i >= 0)
+            {
+                Console.Write("{0}", array[i]);
+                if (i == 0)
+                {
+                    Console.Write("\n");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+                i--;
+            }
+        }
+
+    }
+}
