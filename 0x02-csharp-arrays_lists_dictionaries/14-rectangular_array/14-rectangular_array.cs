d492dd3d972585bae32f81d0ce9de7e26f1d99b8
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 22:26:54 -0600

    task 14

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 5];
+            int row;
+            int col;
+
+            for (row = 0; row < 5; row++)
+            {
+                for (col = 0; col < 5; col++)
+                {
+                    if ((row == 2) && (col == 2))
+                    {
+                        matrix[row, col] = 1;
+                        Console.Write("1");
+                    }
+                    else
+                    {
+                        matrix[row, col] = 0;
+                        Console.Write("0");
+                    }
+                    if (col == 4)
+                    {
+                        Console.Write("\n");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+            }
+        }
+    }
+}
