c378d3afab7c62d8cfb8b59f0bdc902950e6eb3f
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-02 22:58:47 -0600

    task 100

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] newArray = new int[3][];
+            newArray[0] = new int[] {0, 1, 2, 3};
+            newArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            newArray[2] = new int[] {0, 1};
+            Console.WriteLine("0 1 2 3");
+            Console.WriteLine("0 1 2 3 4 5 6");
+            Console.WriteLine("0 1");
+        }
+    }
+}
