ae3442118481fc4b73a71604d1696bebba9fc052
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 17:51:26 -0600

    task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+        if (size > -1)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                newList.AddLast(i);
+                Console.WriteLine(i);
+            }
+        }
+        return newList;
+    }
+}
