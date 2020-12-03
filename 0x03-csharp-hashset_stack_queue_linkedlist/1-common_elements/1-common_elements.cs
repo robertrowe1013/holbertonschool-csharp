3f65c6f00d9bdc28d6998f2f6bf3de9c32fe5801
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 05:30:25 -0600

    task 1

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> common = new List<int>();
+        list1.Sort();
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list2.Contains(list1[i]))
+            {
+                common.Add(list1[i]);
+            }
+        }
+        return common;
+    }
+}
