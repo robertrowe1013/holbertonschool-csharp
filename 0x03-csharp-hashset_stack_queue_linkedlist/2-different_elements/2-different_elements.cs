2829cd9ae9444abd9a79081bf5a8d2e050076498
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 06:03:15 -0600

    task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> uncommon = new List<int>();
+        for (int i = 0; i < list1.Count; i++)
+        {
+            if (list2.Contains(list1[i]))
+            {
+                continue;
+            }
+            else
+            {
+                uncommon.Add(list1[i]);
+            }
+        }
+        for (int i = 0; i < list2.Count; i++)
+        {
+            if (list1.Contains(list2[i]))
+            {
+                continue;
+            }
+            else
+            {
+                uncommon.Add(list2[i]);
+            }
+        }
+        uncommon.Sort();
+        return uncommon;
+    }
+}
