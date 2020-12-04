242b123e1ba859ab9a73af6b0d04f9c6294d82a6
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 21:25:08 -0600

    task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index == (myLList.Count - 1))
+        {
+            myLList.RemoveLast();
+        }
+        else
+        {
+            var node = myLList.First;
+            for (int i = 0; i < index; i++)
+            {
+                node = node.Next;
+            }
+            myLList.Remove(node);
+        }
+    }
+}
