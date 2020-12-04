b2c3b81cfc83aa29f3912e91d5316ce77265cdf6
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 21:16:17 -0600

    task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        var node = myLList.First;
+        if ((myLList.Count == 0) || (n < node.Value))
+        {
+            myLList.AddFirst(n);
+            return node;
+        }
+        while (node.Next != null)
+        {
+            if (n < node.Value)
+            {
+                myLList.AddBefore(node, n);
+                return node.Previous;
+            }
+            node = node.Next;
+        }
+        myLList.AddLast(n);
+        return myLList.Last;
+    }
+}
