5d204c6f34671259990b000eadb9b88bb50d33b4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 20:58:32 -0600

    task 11

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int sum = 0;
+        var node = myLList.First;
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            sum += node.Value;
+            node = node.Next;
+        }
+        return sum;
+    }
+}
