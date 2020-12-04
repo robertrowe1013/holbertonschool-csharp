fd3195b774757e6d8d1741f1055ba6efee97ec09
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 20:37:18 -0600

    task 10

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (n >= myLList.Count)
+        {
+            return 0;
+        }
+        int val = 0;
+        var node =myLList.First;
+        for (int i = 0; i <= n; i++)
+        {
+            val = node.Value;
+            node = node.Next;
+        }
+        return val;
+    }
+}
