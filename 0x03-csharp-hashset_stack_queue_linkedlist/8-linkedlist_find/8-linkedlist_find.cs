45bf2a9d49c1d79e3b6a105efcfbead67598d8fa
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 19:30:28 -0600

    task 8

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int counter = 0;
+        foreach (int i in myLList)
+        {
+            if (i == value)
+            {
+                break;
+            }
+            counter++;
+        }
+        if (counter == myLList.Count)
+        {
+            return (-1);
+        }
+        return (counter);
+    }
+}
