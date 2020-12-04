658100da3f6f7ceac2787c441859ea3d4c56bf27
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 18:00:21 -0600

    task 6

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int listCount = 0;
+        foreach(int i in myLList)
+        {
+            listCount++;
+        }
+        return listCount;
+    }
+}
