2a9ef1a7ead7e841d67b37d0583e05ca61714de4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-03 21:31:56 -0600

    corrected last index check

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -5,7 +5,7 @@ class LList
 {
     public static void Delete(LinkedList<int> myLList, int index)
     {
-        if (index == (myLList.Count - 1))
+        if (index == (myLList.Count))
         {
             myLList.RemoveLast();
         }
