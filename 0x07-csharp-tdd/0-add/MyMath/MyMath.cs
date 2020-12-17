bdd1f26702d5a37b6fcfb749262ee8a0ab581ccc
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 09:49:00 -0600

    comments

--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,8 +2,14 @@
 
 namespace MyMath
 {
+    /// <summary>
+    /// Maths
+    /// </summary>
     public class Operations
     {
+        /// <summary>
+        /// add
+        ///</summary>
         public static int Add(int a, int b)
         {
             int sum;
