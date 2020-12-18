3bcd2fa8e988ca8dd830d085b521cb6c5621c8e4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 20:00:29 -0600

    task 1

---
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>
+    /// maths
+    /// </summary>
+    public class Matrix
+    {
+        /// <summary>
+        /// divide matrix
+        /// </summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            if (matrix == null)
+            {
+                return (null);
+            }
+            int len0 = matrix.GetLength(0);
+            int len1 = matrix.GetLength(1); 
+            int[,] newMatrix = new int[len0, len1];
+
+            for (int i = 0; i < len0; i++)
+            {
+                for (int i2 = 0; i2 < len1; i2++)
+                {
+                    try
+                    {
+                        newMatrix[i, i2] = newMatrix[i, i2] / num;
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        Console.WriteLine("Num cannot be 0");
+                    }
+                }
+            }
+
+            return (newMatrix);
+        }
+    }
+}
