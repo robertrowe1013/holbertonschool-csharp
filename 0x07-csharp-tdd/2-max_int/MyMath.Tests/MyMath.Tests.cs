deddbd2228c41ac0ee61da713d592871ef6dfc95
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 22:23:25 -0600

    test unordered

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -35,5 +35,18 @@ namespace Tests
             int testRes = MyMath.Operations.Max(testList);
             Assert.AreEqual(testRes, 0);
         }
+
+        [Test]
+        public void TestMixed()
+        {
+            List<int> testList = new List<int>();
+            testList.Add(2);
+            testList.Add(0);
+            testList.Add(1);
+            testList.Add(-1);
+
+            int testRes = MyMath.Operations.Max(testList);
+            Assert.AreEqual(testRes, 2);
+        }
     }
 }
