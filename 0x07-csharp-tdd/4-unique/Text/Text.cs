e52640e2ae3ae5dcfa1ab1d24b37ad8f41a3a876
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 23:05:44 -0600

    task 4

---
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// words
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// unique
+        /// </summary>
+        public static int UniqueChar(string s)
+        {
+            if (s == null || s.Length ==0)
+            {
+                return (-1);
+            }
+            int num;
+            for (int i = 0; i < s.Length; i++)
+            {
+                num = 0;
+                for (int i2 = 0; i2 < s.Length; i2++)
+                {
+                    if (s[i] == s[i2])
+                    {
+                        num++;
+                    }
+                }
+                if (num == 1)
+                {
+                    return (i);
+                }
+            }
+            return (-1);
+        }
+    }
+}
