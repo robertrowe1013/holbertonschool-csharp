68ea11c0eb2df6a315ec927083305663ef468329
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 22:56:57 -0600

    task 3

---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Text
+{
+    /// <summary>
+    /// words
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// palindrome
+        /// </summary>
+        public static bool IsPalindrome(string s)
+        {
+            if (s == null)
+            {
+                return (false);
+            }
+            if (s == "")
+            {
+                return (true);
+            }
+            string newS = s.ToLower();
+            newS = Regex.Replace(newS, "[^a-z]*", "");
+            char[] charArray = newS.ToCharArray();
+            Array.Reverse(charArray);
+            string reverseS = new string(charArray);
+            if (newS == reverseS)
+            {
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+    }
+}
