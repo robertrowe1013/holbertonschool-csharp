6f67381ae564811beb73653f8fbf28a5db743456
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-17 23:11:31 -0600

    task 5

---
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// words
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// word count
+        /// </summary>
+        public static int CamelCase(string s)
+        {
+            if (s == null || s.Length == 0)
+            {
+                return (0);
+            }
+            int count = 1;
+            foreach (char i in s)
+            {
+                if (i >= 65 && i <= 90)
+                {
+                    count++;
+                }
+            }
+            return (count);
+        }
+    }
+}
