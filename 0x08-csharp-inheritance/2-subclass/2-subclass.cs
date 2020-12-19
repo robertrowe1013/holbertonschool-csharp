c7faa34c98bd3f8b1db9bbabd42723d0c20bee6f
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-19 10:07:27 -0600

    task 2

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>
+    /// check subclass
+    /// </summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType.IsSubclassOf(baseType));
+    }
+}
