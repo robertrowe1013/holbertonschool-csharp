f09f2573de0aef8e94a499c6c4d4bdba9af8f63b
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-19 09:46:21 -0600

    task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary>
+    /// Check obj type
+    /// </summary>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+        {
+            return (true);
+        }
+        return (false);
+    }
+}
