329665f3ab3d6e6a42bdad53a5e1397e15161695
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-19 09:54:30 -0600

    task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Obj
+{
+    /// <summary>
+    /// check obj inheritance
+    /// </summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+        {
+            return (true);
+        }
+        return (false);
+    }
+}
