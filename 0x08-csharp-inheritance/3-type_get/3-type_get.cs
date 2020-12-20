5f9c6f17cfa261b6bbb91575d7f9b66d13c88b23
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-20 08:03:33 -0600

    task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+class Obj
+{
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] props = myObj.GetType().GetProperties();
+        MethodInfo[] methods = myObj.GetType().GetMethods();
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach (PropertyInfo prop in props)
+        {
+            Console.WriteLine(prop.Name);
+        }
+
+        foreach (MethodInfo method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
