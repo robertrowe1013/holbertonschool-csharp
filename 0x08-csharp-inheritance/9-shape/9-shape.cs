177fe7bbc5cc743efa760f1bf009fe6e219ddf83
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2020-12-21 12:58:18 -0600

    task 9

---
+++ 0x08-csharp-inheritance/9-shape/9-shape.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary>
+    /// Shape Area
+    /// </summary>
+    public virtual int Area()
+    {
+        string msg = "Area() is not implemented";
+        throw new NotImplementedException(msg);
+    }
+}
+
+class Rectangle : Shape
+{
+    /// <summary>
+    /// retangle shape
+    /// </summary>
+    private int width;
+    public int Width
+    {
+        get
+        {
+            return (width);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        }
+    }
+    private int height;
+    public int Height
+    {
+        get
+        {
+            return (height);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        }
+    }
+    public new int Area()
+    {
+        return (height * width);
+    }
+    public override string ToString()
+    {
+        string str = "[Rectangle] " + width + " / " + height;
+        return (str);
+    }
+}
+
+class Square : Rectangle
+{
+    /// <summary>
+    /// square
+    ///</summary>
+    private int size;
+    public int Size
+    {
+        get
+        {
+            return (size);
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            }
+            else
+            {
+                size = value;
+                Height = value;
+                Width = value;
+            }
+        }
+    }
+    public override string ToString()
+    {
+        string str = "[Square] " + size + " / " + size;
+        return (str);
+    }
+}
