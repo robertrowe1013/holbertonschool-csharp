68cba0308cabe0a1f23775bdc8cb650b2365834e
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-18 03:46:05 -0600

    task 18 start

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+///  matrix math
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(1) == matrix2.GetLength(0))
+        {
+            double[,] newMatrix = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+            return (newMatrix);
+        }
+        else
+        {
+            double[,] newMatrix = new double[] {{-1}};
+            return (newMatrix);
+        }
+    }
+}
