46f1bd3dca9b392b58f2e18f8eea405a6705f309
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-18 00:04:06 -0600

    task 14 start

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+///  matrix math
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) == 2)
+        {
+            double[,] matrix = new double[,] {-1};
+            return (matrix);
+        }
+        else if (matrix1.GetLength(0) == 3)
+        {
+            double[,] matrix = new double[,] {-1};
+            return (matrix);
+        }
+        else
+        {
+            double[,] matrix = new double[,] {-1};
+            return (matrix);
+        }
+    }
+}
