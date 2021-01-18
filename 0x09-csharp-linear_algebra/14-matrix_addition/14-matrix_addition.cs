acab6924d06726b5545aa5908f7f5841758bcbfb
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-18 00:40:42 -0600

    task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,11 +10,28 @@ class MatrixMath
         if (matrix1.GetLength(0) == 2)
         {
             double[,] matrix = new double[,] {{0, 0}, {0, 0}};
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+                }
+            }
             return (matrix);
         }
         else if (matrix1.GetLength(0) == 3)
         {
             double[,] matrix = new double[,] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        matrix[i][j][k] = matrix1[i][j][k] + matrix2[i][j][k];
+                    }
+                }
+            }
             return (matrix);
         }
         else
