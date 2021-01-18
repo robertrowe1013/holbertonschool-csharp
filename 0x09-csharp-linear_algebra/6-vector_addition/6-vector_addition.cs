f4d809f897738aa6f6a7e3273118573610389692
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 18:50:42 -0600

    test vector return

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -7,17 +7,18 @@ class VectorMath
 {
     public static double[] Add(double[] vector1, double[] vector2)
     {
+        double[] v = new double[] {-1};
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            return (1);
+            return (v);
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            return (1);
+            return (v);
         }
         else
         {
-            return (-1);
+            return (v);
         }
     }
 }
