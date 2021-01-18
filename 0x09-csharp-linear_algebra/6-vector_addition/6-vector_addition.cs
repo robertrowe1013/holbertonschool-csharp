bcd31837f58093a76af84ad4b6d135cf0aab5157
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 19:15:35 -0600

    vector size

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -7,15 +7,16 @@ class VectorMath
 {
     public static double[] Add(double[] vector1, double[] vector2)
     {
-        double[] v = new double[] {-1};
         if (vector1.Length == 2 && vector2.Length == 2)
         {
+            double[] v = new double[] {0, 0};
             v[0] = Math.Round((vector1[0] + vector2[0]), 2);
             v[1] = Math.Round((vector1[1] + vector2[1]), 2);
             return (v);
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
+            double[] v = new double[] {0, 0, 0};
             v[0] = Math.Round((vector1[0] + vector2[0]), 2);
             v[1] = Math.Round((vector1[1] + vector2[1]), 2);
             v[2] = Math.Round((vector1[2] + vector2[2]), 2);
@@ -23,6 +24,7 @@ class VectorMath
         }
         else
         {
+            double[] v = new double[] {-1};
             return (v);
         }
     }
