ceadcc9866e56bf124d897de52e3bb7f704083a4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 18:32:50 -0600

    task 6 start

---
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+///  Vector math
+/// </summary>
+class VectorMath
+{
+    public static double[] Add(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            return (1);
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            return (1);
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+}
