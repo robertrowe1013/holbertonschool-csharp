7946769f356833bbe9cb533a012f5da27eac160a
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 18:56:14 -0600

    rounding

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -10,15 +10,15 @@ class VectorMath
         double[] v = new double[] {-1};
         if (vector1.Length == 2 && vector2.Length == 2)
         {
-            v[0] = vector1[0] + vector2[0];
-            v[1] = vector1[1] + vector2[1];
+            v[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            v[1] = Math.Round((vector1[1] + vector2[1]), 2);
             return (v);
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
-            v[0] = vector1[0] + vector2[0];
-            v[1] = vector1[1] + vector2[1];
-            v[2] = vector1[2] + vector2[2];
+            v[0] = Math.Round((vector1[0] + vector2[0]), 2);
+            v[1] = Math.Round((vector1[1] + vector2[1]), 2);
+            v[2] = Math.Round((vector1[2] + vector2[2]), 2);
             return (v);
         }
         else
