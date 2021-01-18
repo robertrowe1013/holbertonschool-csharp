7e30f8dc0a6459bb0a8e0946cffa6820905a49c1
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-18 01:15:27 -0600

    task 14

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -9,26 +9,26 @@ class MatrixMath
     {
         if (matrix1.GetLength(0) == 2 && matrix2.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2)
         {
-            double[,] matrix = new double[,] {{0, 0}, {0, 0}};
+            double[,] matrix = new double[2, 2];
             for (int i = 0; i < 2; i++)
             {
                 for (int j = 0; j < 2; j++)
                 {
-                    matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+                    matrix[i, j] = matrix1[i, j] + matrix2[i, j];
                 }
             }
             return (matrix);
         }
         else if (matrix1.GetLength(0) == 3 && matrix2.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3)
         {
-            double[,] matrix = new double[,] {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+            double[,] matrix = new double[3, 3];
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     for (int k = 0; k < 3; k++)
                     {
-                        matrix[i][j][k] = matrix1[i][j][k] + matrix2[i][j][k];
+                        matrix[i, j, k] = matrix1[i, j, k] + matrix2[i, j, k];
                     }
                 }
             }
