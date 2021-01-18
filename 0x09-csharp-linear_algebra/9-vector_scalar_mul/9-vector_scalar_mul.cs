e138631def180b4a65a61f18ad101e5c1d6efe50
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 22:15:57 -0600

    task 9

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+///  Vector math
+/// </summary>
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length == 2)
+        {
+            double[] v = new double[] {0, 0};
+            v[0] = Math.Round((vector[0] * scalar), 2);
+            v[1] = Math.Round((vector[1] * scalar), 2);
+            return (v);
+        }
+        else if (vector.Length == 3)
+        {
+            double[] v = new double[] {0, 0, 0};
+            v[0] = Math.Round((vector[0] * scalar), 2);
+            v[1] = Math.Round((vector[1] * scalar), 2);
+            v[2] = Math.Round((vector[2] * scalar), 2);
+            return (v);
+        }
+        else
+        {
+            double[] v = new double[] {-1};
+            return (v);
+        }
+    }
+}
