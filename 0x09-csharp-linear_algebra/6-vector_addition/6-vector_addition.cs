0bff17ee6413d506403526687c257848ba73223a
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 18:53:24 -0600

    task 6

--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 /// <summary>
-///  Vectormath
+///  Vector math
 /// </summary>
 class VectorMath
 {
@@ -10,10 +10,15 @@ class VectorMath
         double[] v = new double[] {-1};
         if (vector1.Length == 2 && vector2.Length == 2)
         {
+            v[0] = vector1[0] + vector2[0];
+            v[1] = vector1[1] + vector2[1];
             return (v);
         }
         else if (vector1.Length == 3 && vector2.Length == 3)
         {
+            v[0] = vector1[0] + vector2[0];
+            v[1] = vector1[1] + vector2[1];
+            v[2] = vector1[2] + vector2[2];
             return (v);
         }
         else
