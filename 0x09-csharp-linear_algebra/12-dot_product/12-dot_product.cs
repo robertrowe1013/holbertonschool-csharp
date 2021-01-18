885ec274472e28bceec7b756b060af0ee66bdf80
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-17 23:19:07 -0600

    task 12

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+///  Vector math
+/// </summary>
+class VectorMath
+{
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.Length == 2 && vector2.Length == 2)
+        {
+            return (Math.Round(((vector1[0] * vector2[0]) + (vector1[1] * vector2[1])), 2));
+        }
+        else if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] v = new double[] {0, 0, 0};
+            return (Math.Round(((vector1[0] * vector2[0]) + (vector1[1] * vector2[1]) + (vector1[2] * vector2[2])), 2));
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+}
