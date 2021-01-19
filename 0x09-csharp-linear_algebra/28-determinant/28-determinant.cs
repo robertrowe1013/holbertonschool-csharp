e2937e7b2d032e416e9579af083334166afbd1b1
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 17:25:15 -0600

    test

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,7 +10,7 @@ class MatrixMath
         int det = -1;
         if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-//            det = (matrix[0][0] * matrix [1][1]) - (matrix[0][1] * matrix[1][0]);
+            det = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0]);
             return (det);
         }
         else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
