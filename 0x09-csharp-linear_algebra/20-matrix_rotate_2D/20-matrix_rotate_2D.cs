9ffba46305480790a046c63a4f79dd783fe6d829
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 02:46:45 -0600

    task 20 start

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+///  matrix math
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] newMatrix = new double[,] {{-1}};
+        return (newMatrix);
+    }
+}
