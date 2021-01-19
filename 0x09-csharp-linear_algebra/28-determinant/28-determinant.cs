f4b1a30771abfb217d43cc73bc1f5eedd950c3ca
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 17:19:23 -0600

    test

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -7,19 +7,20 @@ class MatrixMath
 {
     public static double Determinant(double[,] matrix)
     {
+        int det = -1;
         if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            int det = (matrix[0][0] * matrix [1][1]) - (matrix[0][1] * matrix[1][0]);
+//            det = (matrix[0][0] * matrix [1][1]) - (matrix[0][1] * matrix[1][0]);
             return (det);
         }
         else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
         {
-//            int det = (((matrix[0][0] * matrix[1][1] * matrix[2][2]) + (matrix[0][1] * matrix[1][2] * matrix[2][0]) + (matrix[0][2] * matrix[1][0] * matrix[2][1])) - ((matrix[0][2] * matrix[1][1] * matrix[2][0]) - (matrix[0][1] * matrix[1][0] * matrix[2][2]) - (matrix[0][0] * matrix[1][2] * matrix[2][1])));
-            return (-1);
+//            det = (((matrix[0][0] * matrix[1][1] * matrix[2][2]) + (matrix[0][1] * matrix[1][2] * matrix[2][0]) + (matrix[0][2] * matrix[1][0] * matrix[2][1])) - ((matrix[0][2] * matrix[1][1] * matrix[2][0]) - (matrix[0][1] * matrix[1][0] * matrix[2][2]) - (matrix[0][0] * matrix[1][2] * matrix[2][1])));
+            return (det);
         }
         else
         {
-            return (-1);
+            return (det);
         }
     }
 }
