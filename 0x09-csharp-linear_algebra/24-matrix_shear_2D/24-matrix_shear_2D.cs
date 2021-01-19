b44a15055f3d8384cf7a4c7a365f4e1389721ce2
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 03:29:18 -0600

    task 24 start

---
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+///  matrix math
+/// </summary>
+class MatrixMath
+{
+    public static double[,] Shear2D(double[,] matrix, char direction, double factor)
+    {
+        double[,] newMatrix = new double[,] {{-1}};
+        return (newMatrix);
+    }
+}
