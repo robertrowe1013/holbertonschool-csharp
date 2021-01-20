fe511376b6ac79cde4b0364eec74820b6d4ec99c
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 21:06:30 -0600

    task 30

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -7,7 +7,17 @@ class VectorMath
 {
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        double[] v = new double[] {-1};
-        return (v);
+        if (vector1.Length == 3 && vector2.Length == 3)
+        {
+            double[] v = new double[] {0, 0, 0};
+            v[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);
+            v[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);
+            v[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);
+        }
+        else
+        {
+            double[] v = new double[] {-1};
+        }
+        return (Math.Round(v, -2));
     }
 }
