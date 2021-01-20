f8a9dabce97703cc45cfa1aac1950fef92bd8e79
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-19 20:27:38 -0600

    task 28

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -15,7 +15,7 @@ class MatrixMath
         }
         else if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3)
         {
-            det = (((matrix[0, 0] * matrix[1, 1] * matrix[2, 2]) + (matrix[0, 1] * matrix[1, 2] * matrix[2, 0]) + (matrix[0, 2] * matrix[1, 0] * matrix[2, 1])) - ((matrix[0, 2] * matrix[1, 1] * matrix[2, 0]) - (matrix[0, 1] * matrix[1, 0] * matrix[2, 2]) - (matrix[0, 0] * matrix[1, 2] * matrix[2, 1])));
+            det = ((matrix[0, 0] * ((matrix[1, 1] * matrix[2, 2]) - (matrix[1, 2] * matrix[2, 1]))) - (matrix[0, 1] * ((matrix[1, 0] * matrix[1, 1]) - (matrix[1, 2] * matrix[2, 0]))) + (matrix[0, 2] * ((matrix[1, 0] * matrix[2, 1]) - (matrix[1, 1] * matrix[2, 0]))));
             return (Math.Round(det,2));
         }
         else
