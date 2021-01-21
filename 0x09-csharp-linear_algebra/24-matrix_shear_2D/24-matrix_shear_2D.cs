84b9b9e5e9e98f68ce3408c317398a28b3632b80
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-01-20 19:46:22 -0600

    task 24

--- 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ 0x09-csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -7,7 +7,30 @@ class MatrixMath
 {
     public static double[,] Shear2D(double[,] matrix, char direction, double factor)
     {
-        double[,] newMatrix = new double[,] {{-1}};
-        return (newMatrix);
+        if (matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2 && (direction == 'x' || direction == 'y'))
+        {
+            double[,] newMatrix = new double[2, 2];
+            if (direction == 'x')
+            {
+                newMatrix[0, 0] = matrix[0, 1] * factor + matrix[0, 0];
+                newMatrix[1, 0] = matrix[1, 1] * factor + matrix[1, 0];
+                newMatrix[0, 1] = matrix[0, 1];
+                newMatrix[1, 1] = matrix[1, 1];
+                return (newMatrix);
+            }
+            else
+            {
+                newMatrix[0, 1] = matrix[0, 0] * factor + matrix[0, 1];
+                newMatrix[1, 1] = matrix[1, 0] * factor + matrix[1, 1];
+                newMatrix[0, 0] = matrix[0, 0];
+                newMatrix[1, 0] = matrix[1, 0];
+                return (newMatrix);
+            }
+        }
+        else
+        {
+            double[,] newMatrix = new double[,] {{-1}};
+            return (newMatrix);
+        }
     }
 }
