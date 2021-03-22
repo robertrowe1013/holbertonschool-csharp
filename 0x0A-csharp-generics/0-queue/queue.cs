fc037151889401cf4a843cdd60332d29677e3e55
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-22 10:59:59 -0500

    task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>
+/// Generic Queue Class
+/// </summary>
+class Queue<T>
+{
+    // check type
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+}
