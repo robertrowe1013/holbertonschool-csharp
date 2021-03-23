3eaa882eb4ab6099db07da6ab9fa6dbae611e51f
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-23 12:12:32 -0500

    test

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -10,6 +10,6 @@ abstract class Base
     // ToString override
     public override string ToString()
     {
-        return (String.Format("{0} is a {1}", name, this.GetType()));
+        return (String.Format("{0} is a {1}", name, this.GetType().ToString()));
     }
 }
