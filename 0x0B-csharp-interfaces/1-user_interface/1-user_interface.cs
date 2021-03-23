02c9fc553742be400e489a1eb9cb9179282bd503
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-23 11:19:03 -0500

    task 1

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// <summary>
+/// Task 1 interface
+/// </summary>
+abstract class Base
+{
+    public string name;
+
+    // ToString override
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact() {}
+    public void Break() {}
+    public void Collect() {}
+}
