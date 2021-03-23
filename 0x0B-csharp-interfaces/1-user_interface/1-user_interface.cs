154fd28d036557e857e101eee32ed8c26cc9365d
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-23 12:19:19 -0500

    name order?

--- 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -35,10 +35,10 @@ interface ICollectable
 
 class TestObject : Base, IInteractive, IBreakable, ICollectable
 {
-    public string name { get; set; }
     public int durability { get; set; }
     public bool isCollected { get; set; }
-
+    public string name { get; set; }
+    
     public void Interact() {}
     public void Break() {}
     public void Collect() {}
