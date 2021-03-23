271dcdbfa5700f6bf979c909638a68df05fc16ca
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-22 22:47:03 -0500

    task 3

---
+++ 0x0A-csharp-generics/3-peek/queue.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+/// <summary>
+/// Generic Queue Class
+/// </summary>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    // check type
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+    // Node class
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    // Enqueue method
+    public void Enqueue(T value)
+    {
+        Node addNode = new Node(value);
+
+        if (head == null)
+        {
+            head = addNode;
+            tail = head;
+        }
+        else
+        {
+            tail.next = addNode;
+            tail = tail.next;
+        }
+        count++;
+    }
+
+    // count method
+    public int Count()
+    {
+        return (count);
+    }
+
+    // dequeue method
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        T value = head.value;
+        head = head.next;
+        count--;
+        return (value);
+    }
+
+    // peek method
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return (head.value);
+    }
+}
