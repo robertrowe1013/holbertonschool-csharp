87302b0772fd21fc41946b4bb13246be4578ecab
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-22 21:26:48 -0500

    task 1 first pass

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/// <summary>
+/// Generic Queue Class
+/// </summary>
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count;
+
+    // check type
+    public Type CheckType()
+    {
+        return (typeof(T));
+    }
+    // Node class
+    public class Node
+    {
+        public T value;
+        public Node next = null;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    // Enqueue method
+    public void Enqueue(T value)
+    {
+        Node addNode = new Node(value);
+
+        if (head == null)
+        {
+            head = addNode;
+        }
+        tail = addNode;
+    }
+
+    // count method
+    public int Count()
+    {
+        while (head != null)
+        {
+            count++;
+            head = head.next;
+        }
+        return (count);
+    }
+}
