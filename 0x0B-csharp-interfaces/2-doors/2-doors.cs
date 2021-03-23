bd63430b7c47194c26d8ec4da950142aff33e4ac
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-23 11:52:12 -0500

    task 2

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+/// <summary>
+/// Task 1 interface
+/// </summary>
+abstract class Base
+{
+    public string name;
+
+    // ToString override
+    public override string ToString()
+    {
+        return (name + " is a " + this.GetType());
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+
+    public Door(String name)
+    {
+        if (name == null)
+        {
+            this.name = "Door";
+        }
+        else
+        {
+            this.name = name;
+        }
+    }
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + name + ". It's locked.");
+    }
+}
