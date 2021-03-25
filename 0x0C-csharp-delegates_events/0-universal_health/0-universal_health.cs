00bb7649d1954400a92aebfefaad273c6ea22c61
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-25 18:26:06 -0500

    min health

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -15,7 +15,7 @@ public class Player
     public Player(string name = "Player", float maxHp = 100f)
     {
         this.name = name;
-        if (maxHp < 0)
+        if (maxHp < 1)
         {
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             this.maxHp = 100f;
