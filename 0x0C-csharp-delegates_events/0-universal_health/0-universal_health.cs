d2a911064ef55fb790a65ed78e8d4c2f8496b0d4
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-25 17:56:06 -0500

    task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+///<summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    string name { get; set; }
+    float maxHp { get; set; }
+    float hp { get; set; }
+
+    /// <summary>
+    /// player constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp < 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+            this.hp = maxHp;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    /// <summary>
+    /// print health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
