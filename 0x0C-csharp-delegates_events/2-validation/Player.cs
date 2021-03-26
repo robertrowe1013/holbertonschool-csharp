2c4c2d0d13ec62167dbe46cf97b25017985983b7
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-03-25 19:43:38 -0500

    task 2

---
+++ 0x0C-csharp-delegates_events/2-validation/Player.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+///<summary>
+/// Player class
+/// </summary>
+public class Player
+{
+    string name { get; set; }
+    float maxHp { get; set; }
+    float hp { get; set; }
+
+    /// <summary>
+    /// player constructor
+    /// </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp < 1)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+            this.hp = maxHp;
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    /// <summary>
+    /// print health
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    /// <summary>
+    /// take damage
+    /// </summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+        {
+            damage = 0;
+        }
+        Console.WriteLine($"{name} takes {damage} damage!");
+        ValidateHP(hp - damage);
+    }
+
+    /// <summary>
+    /// heal damage
+    /// </summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+        {
+            heal = 0;
+        }
+        Console.WriteLine($"{name} heals {heal} HP!");
+        ValidateHP(hp + heal);
+    }
+
+    /// <summary>
+    /// validate hp
+    /// </summary>
+    public void ValidateHP(float newHp)
+    {
+        if (newHp < 0)
+        {
+            newHp = 0;
+        }
+        if (newHp > maxHp)
+        {
+            newHp = maxHp;
+        }
+        hp = newHp;
+    }
+}
+
+/// <summary>
+/// damage delegate
+/// </summary>
+public delegate void CalculateHealth(float dmg);
