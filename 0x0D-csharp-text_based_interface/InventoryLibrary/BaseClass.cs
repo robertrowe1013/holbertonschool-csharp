1d23e925cd21001d8180ac15e58b306b70c454ea
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-04-01 21:52:25 -0500

    start

---
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    public class BaseClass
+    {
+        string id { get; set; }
+        DateTime date_created { get; set; }
+        DateTime date_updated { get; set; }
+    }
+
+    public class Item : BaseClass
+    {
+        string name { get; set; }
+        string description { get; set; }
+        float price { get; set; }
+        List tags = new List<string>();
+    }
+
+    public class User : BaseClass
+    {
+        string name { get; set; }
+    }
+
+    public class Inventory : BaseClass
+    {
+        id user_id;
+        id item_id;
+        int quantity = 1;
+    }
+}
+
